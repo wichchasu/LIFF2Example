836a961e1c33307612c4dcc8e7742363a2b253ba
wichchasu <70121716+wichchasu@users.noreply.github.com>
2023-07-13 13:27:16 +0700

    Update liff-init.js

--- js/liff-init.js
+++ js/liff-init.js
@@ -1,7 +1,7 @@
 // ----- Configuration Zone! -----
 // Don't forget to change this to your LIFF ID
 
-const liffId = "1653671627-vopabBp5";
+const liffId = "1657801658-71k6mOlO";
 
 // Don't forget to change this to your LIFF ID
 
